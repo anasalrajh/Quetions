812b3e72747632bbdf7cbe5d9eb2dcb451f59ff9
Anas <Anas@DESKTOP-J412U6U>
2022-03-25 11:15:08 +0400

    Add project files.

---
+++ Questions/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Questions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Questions.Controllers
+{
+    public class HomeController : Controller
+    {
+       [HttpGet]
+        public async Task<ActionResult> Index(int? id)
+        {
+
+
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            using (var Client = new HttpClient(handler))
+            {
+                ServicePointManager.Expect100Continue = true;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                Client.BaseAddress = new Uri("https://api.stackexchange.com/");
+                //HTTP GET
+                Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/jason"));
+                var response = Client.GetAsync("2.3/questions?order=desc&sort=activity&site=stackoverflow");
+                response.Wait();
+                var result = response.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var Response = result.Content.ReadAsStringAsync().Result;
+                  
+               
+                    StackResponse GetObjects = JsonConvert.DeserializeObject<StackResponse>(Response);
+                    var get = GetObjects.items.OrderByDescending(x=>x.creation_date).ToList();
+                    if (id != null) { return View("Details",get.SingleOrDefault(x=>x.question_id == id)); }
+                    return View(get);
+                }
+
+            }
+
+            return View();
+        }
+       public ActionResult Details() {
+            return View();
+        }
+
+    }
+}
---
+++ Questions/Models/QuestionsModel.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Questions.Models
+{
+
+   
+    public class StackResponse
+    {
+        [JsonProperty("items")]
+        public List<items> items { get; set; }
+      
+        public Nullable<bool> has_more { get; set; }
+        [JsonProperty("quota_max")]
+        public Nullable<int> quota_max { get; set; }
+        [JsonProperty("quota_remaining")]
+        public Nullable<int> quota_remaining { get; set; }
+    }
+ 
+        public class items
+    {
+        [JsonProperty("tags")]
+        public IEnumerable<string> tags { get; set; }
+       
+        [JsonProperty("is_answered")]
+        public Nullable<bool> is_answered { get; set; }
+        [JsonProperty("view_count")]
+        public Nullable<int> view_count { get; set; }
+        [JsonProperty("answer_count")]
+        public Nullable<int> answer_count { get; set; }
+        
+            [JsonProperty("creation_date")]
+        public string creation_date { get; set; }
+        [JsonProperty("score")]
+        public Nullable<int> score { get; set; }
+       
+        [JsonProperty("question_id")]
+
+        public Nullable<int> question_id { get; set; }
+        [JsonProperty("content_license")]
+        public string content_license { get; set; }
+        [JsonProperty("link")]
+        public string link { get; set; }
+        [JsonProperty("title")]
+        public string title { get; set; }
+      
+    }
+    public class owner
+    {
+       
+     
+        [JsonProperty("reputation")]
+        public Nullable<int> reputation { get; set; }
+        [JsonProperty("user_id")]
+        public Nullable<int> user_id { get; set; }
+        [JsonProperty("user_type")]
+        public string user_type { get; set; }
+        [JsonProperty("accept_rate")]
+        public Nullable<int> accept_rate { get; set; }
+        [JsonProperty("profile_image")]
+        public string profile_image { get; set; }
+        [JsonProperty("display_name")]
+        public string display_name { get; set; }
+        [JsonProperty("link")]
+        public string link { get; set; }
+
+    }
+}
